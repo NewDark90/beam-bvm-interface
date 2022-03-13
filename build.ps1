cea4d4ebcadc97e14cc96dbd06c5d46d1be8097c
Zachary Dow <mr.zachary.dow@gmail.com>
2022-03-12 21:54:30 -0800

    Creates a crate folder based on version

--- build.ps1
+++ build.ps1
@@ -1,30 +1,35 @@
 $originalDir = Get-Location;
+$versionRegex = "(\d+)\.(\d+)\.(\d+)"
+$selectedTag = ""
 
 try {
     Invoke-Expression -Command "git submodule update"
 
-    Write-Output $PSScriptRoot
-
     Set-Location "${PSScriptRoot}\beam"
 
     $tags = Invoke-Expression -Command "git tag --list"
-    
-    $versionRegex = "(\d+)\.(\d+)\.(\d+)"
-
     $tags | Select-String "beam-$versionRegex" | Write-Output 
-
-    $selectedTag = ""
 
     while ($selectedTag -notin $tags)
     {
         $selectedTag = Read-Host "Input a git tag from the beam repository to build"
     }
+    $buildRoot =  "../build/$selectedTag"
 
     Invoke-Expression -Command "git checkout tags/$selectedTag"
-
-    New-Item -ItemType Directory -Force -Path "../build/$selectedTag/src/"
-
+    New-Item -ItemType Directory -Force -Path "$buildRoot/src/"
     Invoke-Expression -Command "bindgen ""./bvm/Shaders/common.h"" -o ""../build/$selectedTag/src/lib.rs"" --no-layout-tests --enable-cxx-namespaces --distrust-clang-mangling -- -x c++ -I ""$Env:BOOST_ROOT"" -I ""./"""
+    Copy-Item -Path "../cargo.bvm-template.toml" -Destination "$buildRoot/cargo.toml" -Force
+
+    $versionMatch = ($selectedTag | Select-String $versionRegex)
+    if ($versionMatch.Matches.Success)
+    {
+        (Get-Content "$buildRoot/cargo.toml").replace('{VERSION}', $versionMatch.Matches) | Set-Content "$buildRoot/cargo.toml"
+    }
+    else 
+    {
+        Write-Host "Could not derive a version from the tag. Cargo.toml does not have a valid version in place."
+    }
 
 }
 catch {
